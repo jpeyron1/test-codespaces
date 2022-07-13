089d745f42b67aa0d2020eea1a69518586ed0cf4
Josselin Peyron <josselin.peyron@maif.fr>
2022-07-13 10:54:01 +0200

    first commit

---
+++ src/test/java/org/jpeyron/hellocodespaces/HelloCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpeyron.hellocodespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
